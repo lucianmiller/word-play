971e9cc9fb968f6bcae2eb95af12b10ef0bdc166
Lucian <lucian.miller185@gmail.com>
2021-05-18 11:12:35 -0700

    It will delete console logs

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,15 @@
 $(document).ready(function() {
   $("form#form").submit(function(event) {
     const userSentence = $("#sentence").val();
-    console.log(userSentence);
     const splitSentence = userSentence.split(" ");
-    console.log(splitSentence);
     const bigWordsArray = [];
     splitSentence.forEach(function (word){
       if(word.length >= 3) {
         bigWordsArray.push(word);
       };
     });
-    console.log(bigWordsArray);
     const reverseArray = bigWordsArray.reverse();
-    console.log(reverseArray);
     const joinArray = reverseArray.join(' ');
-    console.log(joinArray);
 
     $("#output").text(joinArray);
 
