d6c9e14c9e0adb654aee37808246569f4de25502
Lucian <lucian.miller185@gmail.com>
2021-05-18 11:06:25 -0700

    It will split sentence at every space

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
   $("form#form").submit(function(event) {
+    const userSentence = $("#sentence").val();
+    console.log(userSentence);
+    const splitSentence = userSentence.split(" ");
+    console.log(splitSentence);
+    const bigWordsArray = [];
+    splitSentence.forEach(function (word){
+      if(word.length >= 3) {
+        bigWordsArray.push(word);
+      };
+    });
+    console.log(bigWordsArray);
     event.preventDefault();
   });
 });
